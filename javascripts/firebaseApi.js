0517411b05fc679f6e1a0c6973297ba5d7a6db00
ben harrington <harringtonben@icloud.com>
2017-10-31 20:16:32 -0500

    getting data from firebase

--- javascripts/firebaseApi.js
+++ javascripts/firebaseApi.js
@@ -2,6 +2,10 @@
 
 let firebaseKey = '';
 let userUid = '';
+let parkAreas = [];
+let parkAttractions = [];
+let parkAttractionTypes = [];
+let parkInfo = [];
 
 const setKey = (key) => {
     firebaseKey = key;
@@ -14,10 +18,95 @@ let authenticateGoogle = () => {
         .then((authData) => {
             userUid = authData.user.uid;
             resolve(authData.user);
+            dataGetter();
         }).catch((error) => {
             reject(error);
         });
     });
   };
+
+const getParkAreas = () => {
+    return new Promise((resolve, reject) => {
+        $.ajax(`${firebaseKey.databaseURL}/areas.json`).then((areas) => {
+            if (areas != null) {
+                Object.keys(areas).forEach((key) => {
+                    areas[key].id = key;
+                    parkAreas.push(areas[key]);
+                });
+            }
+            resolve(parkAreas);
+        }).catch((error) => {
+            reject(error);
+        });
+    });
+};
+
+const getParkAttractions = () => {
+    return new Promise((resolve, reject) => {
+        $.ajax(`${firebaseKey.databaseURL}/attractions.json`).then((attractions) => {
+            if (attractions != null) {
+                Object.keys(attractions).forEach((key) => {
+                    attractions[key].id = key;
+                    parkAttractions.push(attractions[key]);
+                });
+            }
+            resolve(parkAttractions);
+        }).catch((error) => {
+            reject(error);
+        });
+    });
+};
+
+const getParkAttractionTypes = () => {
+    return new Promise((resolve, reject) => {
+        $.ajax(`${firebaseKey.databaseURL}/attraction_types.json`).then((types) => {
+            if (types != null) {
+                Object.keys(types).forEach((key) => {
+                    types[key].id = key;
+                    parkAttractionTypes.push(types[key]);
+                });
+            }
+            resolve(parkAreas);
+        }).catch((error) => {
+            reject(error);
+        });
+    });
+};
+
+const getParkInfo = () => {
+    return new Promise((resolve, reject) => {
+        $.ajax(`${firebaseKey.databaseURL}/park-info.json`).then((info) => {
+            if (info != null) {
+                Object.keys(info).forEach((key) => {
+                    info[key].id = key;
+                    parkInfo.push(info[key]);
+                });
+            }
+            resolve(parkAreas);
+        }).catch((error) => {
+            reject(error);
+        });
+    });
+};
+
+
+
+const dataGetter = () => {
+    getParkAreas().then((results) => {
+       return getParkAttractions();
+    }).then(() => {
+        getParkAttractions().then((results) => {
+            return getParkAttractionTypes();
+        });
+    }).then(() => {
+        getParkInfo().then((results) => {
+            console.log("parkAreas", parkAreas);
+            console.log("parkAttractions", parkAttractions);
+            console.log("parkAttractionTypes", parkAttractionTypes);
+            console.log("parkInfo", parkInfo); 
+        });
+    });   
+};
+
 
   module.exports = {setKey, authenticateGoogle};
