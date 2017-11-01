2c53d9aaeba3ef46240ecdf343a0e617ccbabad5
ben harrington <harringtonben@icloud.com>
2017-10-31 19:44:08 -0500

    finished firebase google auth

--- javascripts/apiKeys.js
+++ javascripts/apiKeys.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const firebaseApi = require('./firebaseApi');
+
+const apiKeys = () => {
+    return new Promise((resolve, reject) => {
+        $.ajax('./db/apikeys.json').done((data) => {
+            resolve(data.firebaseKeys);
+        }).fail((error) => {
+            reject(error);
+        });
+    });
+};
+
+const retrieveKeys = () => {
+    apiKeys().then((results) => {
+        firebaseApi.setKey(results);
+        firebase.initializeApp(results);
+    }).catch((error) => {
+        console.log('error in retrieve keys', error);
+    });
+};
+
+module.exports = {retrieveKeys};
