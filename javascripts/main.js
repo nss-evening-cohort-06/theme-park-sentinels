c347c0ed3146186bc76a4da32d1e4fef131eb05e
Brooke Lewis <belv2c@gmail.com>
2017-11-04 12:38:05 -0500

    date picker is functional

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,10 +1,13 @@
 'use strict';
 const events = require('./events');
 const apiKeys = require('./apiKeys');
+let moment = require('../lib/node_modules/moment/moment.js');
+moment().format();
 // const copyright = require('./copyright');
 
 
 $(document).ready(function(){
     apiKeys.retrieveKeys();
     events.init();
+    $('#time').bootstrapMaterialDatePicker({ date: false });
 });
