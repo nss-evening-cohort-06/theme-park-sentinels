bfba20253e37b7effebd4870b3fa835a5cb2e2ef
Dre mf Randaci <drerandaci@gmail.com>
2017-11-06 09:44:03 -0600

    working on formatting moment

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,6 +2,7 @@
 const events = require('./events');
 const apiKeys = require('./apiKeys');
 const copyright = require('./copyright');
+const moment = require('../lib/node_modules/moment/moment');
 
 
 $(document).ready(function(){
